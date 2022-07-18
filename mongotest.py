08e2b2a7593ad3a7df78b756f6076f10b3d58874
gursimransingh <gursim211@gmail.com>
2022-07-18 13:03:43 +0530

    this is first push

---
+++ mongotest.py
@@ -0,0 +1,22 @@
+import pymongo
+
+"""
+
+    client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+"""
+
+client = pymongo.MongoClient("mongodb+srv://gursim211:chhabra1714@gursimran.j9i39.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d= {
+    "name":"gursimran",
+    "email":"gursim211@gmail.com",
+    "surname":"singh",
+
+}
+db1 =client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
